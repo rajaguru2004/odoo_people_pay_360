39656515eb4611a7ab6f6c5f7dd68f5e21769220
rajaguru2004 <rajaguru20042@gmail.com>
2026-09-05 18:05:48 +0530

    refactor: remove unused source modules and test files while syncing test data artifacts

--- apps/backend/src/common/decorators/current-user.decorator.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access */
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-
-/** The principal JwtStrategy.validate() put on the request. */
-export const CurrentUser = createParamDecorator(
-  (data: unknown, ctx: ExecutionContext) =>
-    ctx.switchToHttp().getRequest().user,
-);
--- apps/backend/src/overtime-policy/dto/update-overtime-policy.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateOvertimePolicyDto } from './create-overtime-policy.dto';
-
-export class UpdateOvertimePolicyDto extends PartialType(
-  CreateOvertimePolicyDto,
-) {}
